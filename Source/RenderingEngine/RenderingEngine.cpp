6a621c0b71577b088f02e5ff521fc579006f6561
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 03:22:22 -0600

    Update RenderingEngine.cpp

--- Source/RenderingEngine/RenderingEngine.cpp
+++ Source/RenderingEngine/RenderingEngine.cpp
@@ -1,5 +1,7 @@
 #include "RenderingEngine.h"
 #include "Logger.h"
+#include "Shader.h"  // Assuming you have a Shader class
+#include "Camera.h"  // Assuming you have a Camera class
 
 RenderingEngine::RenderingEngine() {
     Logger::log("RenderingEngine constructor called.");
@@ -11,17 +13,40 @@ RenderingEngine::~RenderingEngine() {
 }
 
 bool RenderingEngine::initialize() {
-    // TODO: Implement initialization logic for the rendering engine.
     Logger::log("Initializing rendering engine...");
+
+    // TODO: Initialize the graphics context (e.g., OpenGL, DirectX, etc.)
+
+    // Load default shaders
+    Shader defaultShader;
+    if (!defaultShader.load("path_to_vertex_shader", "path_to_fragment_shader")) {
+        Logger::log("Failed to load default shaders.");
+        return false;
+    }
+    shaders.push_back(defaultShader);
+
+    // Set up the main camera
+    Camera mainCamera;
+    cameras.push_back(mainCamera);
+
+    Logger::log("Rendering engine successfully initialized.");
     return true;
 }
 
-void RenderingEngine::render() {
+void RenderingEngine::render(float interpolationFactor) {
     // TODO: Implement the overall rendering logic.
+    // For now, we'll just log the rendering process.
     Logger::log("Rendering frame.");
+
+    // Render using the main camera and default shader
+    Camera& camera = cameras[0];
+    Shader& shader = shaders[0];
+
+    // TODO: Bind the shader, set camera matrices, render game objects, etc.
 }
 
 void RenderingEngine::cleanup() {
     // TODO: Implement cleanup logic for the rendering engine.
+    // For now, we'll just log the cleanup process.
     Logger::log("Cleaning up rendering engine...");
 }
