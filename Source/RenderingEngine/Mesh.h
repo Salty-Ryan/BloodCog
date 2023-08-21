e5c4869417d2f0c2a7d4792eb3ec8cbea2643bca
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 03:13:00 -0600

    Update Mesh.h

--- Source/RenderingEngine/Mesh.h
+++ Source/RenderingEngine/Mesh.h
@@ -1,13 +1,17 @@
 #pragma once
+#include <string>
 
-class Mesh {
+class Shader {
 public:
-    Mesh();
-    ~Mesh();
+    Shader();  // Constructor.
+    ~Shader();  // Destructor.
 
-    void loadFromOBJ(const char* objPath);  // Load mesh from an OBJ file.
-    void draw();  // Render the mesh.
+    bool load(const std::string& vertexPath, const std::string& fragmentPath);  // Load shader from files.
+    void bind();  // Bind the shader for rendering.
+    void unbind();  // Unbind the shader.
+    void cleanup();  // Clean up shader resources.
 
 private:
-    // Vertex data, normals, UVs, etc.
+    unsigned int shaderID;  // ID for the shader program.
+    // TODO: Add other private member variables for shader attributes, uniform locations, etc.
 };
