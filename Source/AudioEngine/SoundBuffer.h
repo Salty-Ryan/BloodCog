65a1aa02813ec0d26abaf7b72e91ac5d96f6ed4e
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:15:25 -0600

    Update SoundBuffer.h

--- Source/AudioEngine/SoundBuffer.h
+++ Source/AudioEngine/SoundBuffer.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#include <string>
+
 class SoundBuffer {
 public:
     SoundBuffer();
     ~SoundBuffer();
 
     bool loadFromFile(const std::string& filePath);  // Load audio data from a file.
+    void play();  // Play the sound buffer.
+    void stop();  // Stop the sound buffer.
     void release();  // Release the buffer.
 
-    // Additional methods and members as needed.
+private:
+    // TODO: Add private member variables to hold audio data, buffer ID, etc.
+    // For example, if using OpenAL, you might have:
+    // unsigned int bufferID;
 };
