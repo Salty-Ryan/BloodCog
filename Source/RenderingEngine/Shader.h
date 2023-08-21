1602380facea3a82a8bd416721e6ec0e8c8953a3
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 01:46:26 -0600

    Update Shader.h

--- Source/RenderingEngine/Shader.h
+++ Source/RenderingEngine/Shader.h
@@ -1,13 +1,19 @@
 #pragma once
+#include <string>
 
 class Shader {
 public:
-    Shader(const char* vertexPath, const char* fragmentPath);
-    ~Shader();
+    Shader();  // Constructor.
+    ~Shader();  // Destructor.
 
-    void use();  // Activate the shader for rendering.
-    void loadFromFile(const char* vertexPath, const char* fragmentPath);  // Load shader from file.
+    bool load(const std::string& vertexPath, const std::string& fragmentPath);  // Load shader from files.
+    void bind();  // Bind the shader for rendering.
+    void unbind();  // Unbind the shader.
+    void cleanup();  // Clean up shader resources.
 
 private:
-    unsigned int shaderID;
+    // TODO: Add private member variables for shader IDs, uniform locations, etc.
+    // Example:
+    // unsigned int shaderID;
+    // std::map<std::string, int> uniformLocations;
 };
