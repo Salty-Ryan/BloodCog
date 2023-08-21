1a95a462f00c262286edf9c737db1a248087774d
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:09:51 -0600

    Update AudioEngine.cpp

--- Source/AudioEngine/AudioEngine.cpp
+++ Source/AudioEngine/AudioEngine.cpp
@@ -1,54 +1,23 @@
-#include "AudioEngine.h"
-#include "Logger.h"  // Assuming you have a Logger for logging purposes.
+#include "AudioContext.h"
+#include "Logger.h"
 
-AudioEngine::AudioEngine() {
-    Logger::log("AudioEngine constructor called.");
+AudioContext::AudioContext() {
+    Logger::log("AudioContext constructor called.");
 }
 
-AudioEngine::~AudioEngine() {
-    Logger::log("AudioEngine destructor called.");
-    cleanup();  // Ensure cleanup is called when the object is destroyed.
+AudioContext::~AudioContext() {
+    cleanup();
+    Logger::log("AudioContext destructor called.");
 }
 
-bool AudioEngine::initialize() {
-    Logger::log("Initializing audio engine...");
-
-    // Initialize audio engine components.
-    // For example, set up audio context, load audio libraries, etc.
-    
-    // TODO: Add specific initialization code here.
-
-    // If all initializations are successful:
-    Logger::log("Audio engine successfully initialized.");
+bool AudioContext::initialize() {
+    // TODO: Initialize the audio context.
+    Logger::log("AudioContext initialized.");
     return true;
-
-    // If any initialization fails:
-    // Logger::log("Failed to initialize [specific audio component].");
-    // return false;
 }
 
-void AudioEngine::playSound(const Sound& sound) {
-    // Play the provided sound.
-    
-    // TODO: Add sound playing logic here.
-
-    Logger::log("Playing sound: " + sound.getName());  // Assuming Sound has a getName() function.
+void AudioContext::cleanup() {
+    // TODO: Cleanup the audio context.
+    Logger::log("AudioContext cleaned up.");
 }
 
-void AudioEngine::stopSound(const Sound& sound) {
-    // Stop the provided sound.
-    
-    // TODO: Add sound stopping logic here.
-
-    Logger::log("Stopping sound: " + sound.getName());
-}
-
-void AudioEngine::cleanup() {
-    Logger::log("Cleaning up audio engine components...");
-
-    // Clean up resources, deinitialize audio libraries, etc.
-    
-    // TODO: Add specific cleanup code here.
-
-    Logger::log("Audio engine components successfully cleaned up.");
-}
