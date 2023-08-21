a46346c9faef86204873d64a551e27448a06be0f
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:28:28 -0600

    Update FileSystemDataManagement.cpp

--- Source/FileSystemDataManagement/FileSystemDataManagement.cpp
+++ Source/FileSystemDataManagement/FileSystemDataManagement.cpp
@@ -1,13 +1,37 @@
-#include "KeyBindings.h"
+#include "FileSystemDataManagement.h"
+#include "Logger.h"
+#include <fstream>
 
-KeyBindings::KeyBindings() {}
-
-KeyBindings::~KeyBindings() {}
-
-void KeyBindings::bindKey(int keyCode, const char* action) {
-    keyMap[keyCode] = action;
+FileSystemDataManagement::FileSystemDataManagement() {
+    Logger::log("FileSystemDataManagement constructor called.");
 }
 
-const char* KeyBindings::getAction(int keyCode) {
-    return keyMap[keyCode];
+FileSystemDataManagement::~FileSystemDataManagement() {
+    Logger::log("FileSystemDataManagement destructor called.");
+}
+
+bool FileSystemDataManagement::saveGameData(const std::string& filePath, const std::string& data) {
+    std::ofstream outFile(filePath);
+    if (!outFile.is_open()) {
+        Logger::log("Failed to open file for writing: " + filePath);
+        return false;
+    }
+
+    outFile << data;
+    outFile.close();
+    Logger::log("Game data saved to: " + filePath);
+    return true;
+}
+
+std::string FileSystemDataManagement::loadGameData(const std::string& filePath) {
+    std::ifstream inFile(filePath);
+    if (!inFile.is_open()) {
+        Logger::log("Failed to open file for reading: " + filePath);
+        return "";
+    }
+
+    std::string data((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+    inFile.close();
+    Logger::log("Game data loaded from: " + filePath);
+    return data;
 }
