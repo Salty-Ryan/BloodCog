f76157649794a5f998ff0a713b38e6eda6442387
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:08:09 -0600

    Update AudioEngine.h

--- Source/AudioEngine/AudioEngine.h
+++ Source/AudioEngine/AudioEngine.h
@@ -3,6 +3,17 @@
 #include "Sound.h"  // Assuming you have a Sound class for individual sound assets.
 #include "Logger.h"  // For logging purposes.
 
+class SoundBuffer {
+public:
+    SoundBuffer();
+    ~SoundBuffer();
+
+    bool loadFromFile(const std::string& filePath);  // Load audio data from a file.
+    void release();  // Release the buffer.
+
+    // Additional methods and members as needed.
+};
+
 class AudioEngine {
 public:
     AudioEngine();  // Constructor.
