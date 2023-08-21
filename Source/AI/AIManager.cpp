cb25e6dcb75c510ce8f0f7b8d52fe94b13504c6b
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 00:42:21 -0600

    Update AIManager.cpp

--- Source/AI/AIManager.cpp
+++ Source/AI/AIManager.cpp
@@ -1,13 +1,34 @@
 #include "AIManager.h"
+#include "Logger.h"
 
-AIManager::AIManager() {}
-
-AIManager::~AIManager() {}
-
-void AIManager::update() {
-    // Update logic for all AI entities.
+AIManager::AIManager() {
+    Logger::log("AIManager constructor called.");
 }
 
-void AIManager::addEntity(/*Entity or AI type*/) {
-    // Add an AI entity to the list or array.
+AIManager::~AIManager() {
+    cleanup();
+    Logger::log("AIManager destructor called.");
+}
+
+bool AIManager::initialize() {
+    Logger::log("Initializing AI manager...");
+
+    // TODO: Initialization logic.
+
+    Logger::log("AI manager initialized.");
+    return true;
+}
+
+void AIManager::updateAI() {
+    // TODO: AI update logic.
+
+    Logger::log("Updating AI.");
+}
+
+void AIManager::cleanup() {
+    Logger::log("Cleaning up AI manager...");
+
+    // TODO: Cleanup logic.
+
+    Logger::log("AI manager cleaned up.");
 }
