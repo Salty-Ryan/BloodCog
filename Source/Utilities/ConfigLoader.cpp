324f558c5c12b7b480f6682f8fd3569bdfdb5ea0
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:39:25 -0600

    Update ConfigLoader.cpp

--- Source/Utilities/ConfigLoader.cpp
+++ Source/Utilities/ConfigLoader.cpp
@@ -11,6 +11,7 @@ ConfigLoader::~ConfigLoader() {
 
 bool ConfigLoader::loadConfig(const std::string& filePath) {
     // TODO: Load configuration from the given file path.
+    // This might involve reading the file, parsing its contents, and storing the data in the configData map.
 
     Logger::log("Loading configuration from: " + filePath);
     return true;  // Placeholder return.
@@ -18,6 +19,7 @@ bool ConfigLoader::loadConfig(const std::string& filePath) {
 
 void ConfigLoader::saveConfig(const std::string& filePath) {
     // TODO: Save current configuration to the given file path.
+    // This might involve iterating over the configData map and writing the key-value pairs to the file.
 
     Logger::log("Saving configuration to: " + filePath);
 }
