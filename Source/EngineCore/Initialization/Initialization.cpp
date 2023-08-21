7d08311e217b6a4f8aaf810e07280724cc26b5b5
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 01:54:25 -0600

    Update Initialization.cpp

--- Source/EngineCore/Initialization/Initialization.cpp
+++ Source/EngineCore/Initialization/Initialization.cpp
@@ -1,5 +1,5 @@
 #include "Initialization.h"
-#include "Logger.h"  // Assuming you have a Logger for logging purposes.
+#include "Logger.h"
 
 Initialization::Initialization() {
     Logger::log("Initialization constructor called.");
@@ -7,32 +7,18 @@ Initialization::Initialization() {
 
 Initialization::~Initialization() {
     Logger::log("Initialization destructor called.");
-    cleanup();  // Ensure cleanup is called when the object is destroyed.
 }
 
-bool Initialization::setup() {
-    Logger::log("Setting up game engine components...");
+bool Initialization::initializeEngine() {
+    // TODO: Implement engine initialization logic. This might involve setting up rendering context, loading assets, etc.
 
-    // Initialize game engine components.
-    // For example, set up rendering context, initialize libraries, etc.
-    
-    // TODO: Add specific initialization code here.
-
-    // If all initializations are successful:
-    Logger::log("Game engine components successfully initialized.");
-    return true;
-
-    // If any initialization fails:
-    // Logger::log("Failed to initialize [specific component].");
-    // return false;
+    Logger::log("Initializing engine.");
+    return true;  // Placeholder return.
 }
 
-void Initialization::cleanup() {
-    Logger::log("Cleaning up game engine components...");
+void Initialization::shutdownEngine() {
+    // TODO: Implement engine shutdown logic. This might involve releasing resources, saving state, etc.
 
-    // Clean up resources, deinitialize libraries, etc.
-    
-    // TODO: Add specific cleanup code here.
-
-    Logger::log("Game engine components successfully cleaned up.");
+    Logger::log("Shutting down engine.");
 }
+
