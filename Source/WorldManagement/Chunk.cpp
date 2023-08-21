c79e510d6fe4bb867ee090ad087495e744d5baaa
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 12:20:31 -0600

    Update Chunk.cpp

--- Source/WorldManagement/Chunk.cpp
+++ Source/WorldManagement/Chunk.cpp
@@ -1,6 +1,11 @@
 #include "Chunk.h"
 #include "Logger.h"
 
+Chunk::Chunk(unsigned int seed, int chunkX, int chunkY) : chunkSeed(seed), x(chunkX), y(chunkY) {
+    Logger::log("Chunk constructor called.");
+    generateTerrain();
+}
+
 Chunk::Chunk() {
     Logger::log("Chunk constructor called.");
 }
