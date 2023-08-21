ed6f413dcfc2932148f88e03f29e2664cbb49c19
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 00:36:26 -0600

    Update Initialization.h

--- Source/EngineCore/Initialization/Initialization.h
+++ Source/EngineCore/Initialization/Initialization.h
@@ -1,38 +1,10 @@
-#include "Initialization.h"
-#include "Logger.h"  // Assuming you have a Logger for logging purposes.
+#pragma once
 
-Initialization::Initialization() {
-    Logger::log("Initialization constructor called.");
-}
+class Initialization {
+public:
+    Initialization();
+    ~Initialization();
 
-Initialization::~Initialization() {
-    Logger::log("Initialization destructor called.");
-    cleanup();  // Ensure cleanup is called when the object is destroyed.
-}
-
-bool Initialization::setup() {
-    Logger::log("Setting up game engine components...");
-
-    // Initialize game engine components.
-    // For example, set up rendering context, initialize libraries, etc.
-    
-    // TODO: Add specific initialization code here.
-
-    // If all initializations are successful:
-    Logger::log("Game engine components successfully initialized.");
-    return true;
-
-    // If any initialization fails:
-    // Logger::log("Failed to initialize [specific component].");
-    // return false;
-}
-
-void Initialization::cleanup() {
-    Logger::log("Cleaning up game engine components...");
-
-    // Clean up resources, deinitialize libraries, etc.
-    
-    // TODO: Add specific cleanup code here.
-
-    Logger::log("Game engine components successfully cleaned up.");
-}
+    bool setup();  // Sets up the game engine components.
+    void cleanup();  // Cleans up resources before shutting down.
+};
