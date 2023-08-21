987331b960d8602ec8ce0dc88f086f911d44fca3
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 02:15:46 -0600

    Update SoundBuffer.cpp

--- Source/AudioEngine/SoundBuffer.cpp
+++ Source/AudioEngine/SoundBuffer.cpp
@@ -3,6 +3,7 @@
 
 SoundBuffer::SoundBuffer() {
     Logger::log("SoundBuffer constructor called.");
+    // TODO: Initialize member variables.
 }
 
 SoundBuffer::~SoundBuffer() {
@@ -11,12 +12,34 @@ SoundBuffer::~SoundBuffer() {
 }
 
 bool SoundBuffer::loadFromFile(const std::string& filePath) {
-    // TODO: Load audio data from the specified file.
+    // TODO: Load audio data from the specified file into the buffer.
+    // This will depend on the audio library you're using.
+    // For example, with OpenAL, you'd use alBufferData() and related functions.
+
     Logger::log("Loaded sound buffer from: " + filePath);
     return true;
 }
 
+void SoundBuffer::play() {
+    // TODO: Play the sound buffer.
+    // This will depend on the audio library you're using.
+    // For example, with OpenAL, you'd use alSourcePlay() and related functions.
+
+    Logger::log("Playing sound buffer.");
+}
+
+void SoundBuffer::stop() {
+    // TODO: Stop the sound buffer.
+    // This will depend on the audio library you're using.
+    // For example, with OpenAL, you'd use alSourceStop() and related functions.
+
+    Logger::log("Stopping sound buffer.");
+}
+
 void SoundBuffer::release() {
     // TODO: Release the buffer and any associated resources.
+    // This will depend on the audio library you're using.
+    // For example, with OpenAL, you'd use alDeleteBuffers() and related functions.
+
     Logger::log("SoundBuffer released.");
 }
