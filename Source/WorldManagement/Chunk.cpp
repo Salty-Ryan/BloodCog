f61fedbf75b29865c4f85d1978f0e39b51add000
Salty-Ryan <142481049+Salty-Ryan@users.noreply.github.com>
2023-08-21 12:35:16 -0600

    Update Chunk.cpp

--- Source/WorldManagement/Chunk.cpp
+++ Source/WorldManagement/Chunk.cpp
@@ -1,5 +1,7 @@
 #include "Chunk.h"
 #include "Logger.h"
+#include "FastNoiseLite.h"
+
 
 Chunk::Chunk(unsigned int seed, int chunkX, int chunkY) : chunkSeed(seed), x(chunkX), y(chunkY) {
     Logger::log("Chunk constructor called.");
@@ -15,10 +17,21 @@ Chunk::~Chunk() {
 }
 
 void Chunk::generateTerrain() {
-    // TODO: Implement terrain generation for this chunk.
+    FastNoiseLite noise;
+    noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+
+    // Sample the noise at different positions
+    for (int x = 0; x < CHUNK_SIZE; x++) {
+        for (int z = 0; z < CHUNK_SIZE; z++) {
+            float height = noise.GetNoise((float)x, (float)z);
+            // Use the height value to set the terrain height at (x, z)
+            // This is a basic example, and you'll likely want to scale and adjust the noise values to fit your desired terrain shape.
+        }
+    }
 
     Logger::log("Generating terrain for chunk.");
 }
+
 
 void Chunk::render() {
     // TODO: Render the chunk's terrain and any entities within it.
